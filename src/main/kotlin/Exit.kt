51304913f9f29b6589b400d842adf5ba9b11ab15
Данила <duesna897@gmail.com>
2021-10-09 16:41:43 +0300

    Added empty data exit

--- src/main/kotlin/Exit.kt
+++ src/main/kotlin/Exit.kt
@@ -9,3 +9,8 @@ fun exitInvalidArgs() {
     println("Incorrect arguments. Use -h or --help to see possible options.")
     exitProcess(1)
 }
+
+fun exitEmptyData() {
+    println("Empty data. Nothing to draw.")
+    exitProcess(0)
+}
